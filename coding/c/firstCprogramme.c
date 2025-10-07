b3fdc985d722aac09513f3e493c4257ae0c0a8f3
Krish Kumar <xtgodspeed5566@gmail.com>
2025-10-07 18:19:56 +0530

    Add files via upload

---
+++ coding/c/firstCprogramme.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a,b,c;
+//     a = 12;
+//     b = 34;
+//     c = a+b;
+//     printf("the sum of the two no. will be",c);
+    
+//     return(0);
+// }
